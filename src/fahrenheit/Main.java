2b39313936d8e98bf29fe9eb9a6af0bf5d018902
Sena Aybüke <s.aybukeaslan@gmail.com>
2023-01-18 16:12:04 +0100

    Second

---
+++ src/fahrenheit/Main.java
@@ -0,0 +1,10 @@
+package fahrenheit;
+
+public class Main {
+    public static void main(String[] args) {
+        double fh = 100;
+        double cl;
+        cl = ((double) 5/9) *(fh -(double)32);
+        System.out.println(" Girilen sicaklik degeri: " + cl + "celciustur" );
+    }
+}
--- src/second/Main.java
+++ src/second/Main.java
@@ -1,4 +1,4 @@
-package Second;
+package second;
 
 public class Main {
     public static void main(String[] args) {
